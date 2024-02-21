6feb67e67cf078785f2940facd785d40242274b4
Lau <laurentiucozma12@gmail.com>
2024-02-21 18:57:54 +0200

    todo list testing

--- epic-game-news-reactjs/src/App.tsx
+++ epic-game-news-reactjs/src/App.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import TodoItem from "./components/TodoItem";
+import { dummyData } from "./data/todos";
+
 function App() {
+  const [todos, setTodos] = useState(dummyData)
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) => 
+      prevTodos.map((todo) => (todo.id === id ? {...todo, completed } : todo))
+    );
+  }
+
   return (
-    <main className="py-10 bg-red-50 h-screen">
-      <h1>
-        Your Todos
-      </h1>
+    <main className="py-10 bg-gray-600 h-screen space-y-5">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
+        <div className="space-y-2">
+          {dummyData.map(todo => (
+            <TodoItem
+            key={todo.id}
+            todo={todo}
+            onCompletedChange={setTodoCompleted}
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
---
+++ epic-game-news-reactjs/src/components/TodoItem.tsx
@@ -0,0 +1,23 @@
+import { Todo } from "../types/todo";
+
+interface TodoItemProps {
+    todo: Todo;
+    onCompletedChange: (id: number, completed: boolean) => void;
+}
+
+export default function TodoItem({ todo, onCompletedChange }: TodoItemProps) {
+    return(
+        <div>
+            <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white cursor-pointer hover:bg-yellow-400">
+                <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
+                className="scale-125 cursor-pointer" />
+                <span className={todo.completed ? "line-through text-gray-400" : ""}>
+                    {todo.title}
+                </span>
+            </label>
+        </div>
+    )
+}
