f089a22bf8a2eb7daf716b9cbb20f3c8d46a789e
Lau <laurentiucozma12@gmail.com>
2024-02-22 11:15:15 +0200

    project named changed to app

---
+++ app/src/components/AddTodoForm.tsx
@@ -0,0 +1,11 @@
+export default function AddTodoForm() {
+    return(
+        <form action="" className="flex">
+            <input 
+                type="text"
+                placeholder="What needs to be done?"
+                className="rounded-s-md grow border border-gray-400 p-2"
+            />
+        </form>
+    )
+}
