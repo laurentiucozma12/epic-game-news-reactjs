2d6f516dfddead6d37038f756093da5aeae30eb9
Lau <laurentiucozma12@gmail.com>
2024-02-24 23:33:54 +0200

    search bar fixed

--- src/Searchbar.tsx
+++ src/Searchbar.tsx
@@ -1,7 +1,7 @@
 
 function Searchbar() {
     return (
-        <input type="search" className="rounded-[7px] w-40 lg:w-40 xl:w-60 px-1 font-Rubik text-white bg-[#212021] placeholder:text-[#5F5E5F]
+        <input type="search" className="rounded-[7px] w-40 xl:w-48 px-1 font-Rubik text-white bg-[#212021] placeholder:text-[#5F5E5F]
         focus:outline-none focus:border-[#a335ee] focus:ring-4 focus:ring-[#a335ee] duration-700
         ring-4 ring-[#5F5E5F]"
         placeholder="Search" 
