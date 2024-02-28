b7fc2887b81de729659acd10c4384bdcd93bd335
Lau <laurentiucozma12@gmail.com>
2024-02-28 22:48:01 +0200

    route naming changed for faster testing purpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
 import Home from './components/pages/Home';
 import VideoGames from './components/pages/VideoGames';
 import Categories from './components/pages/Categories';
