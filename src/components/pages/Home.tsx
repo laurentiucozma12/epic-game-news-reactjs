e9604cc2ebeabc80ecbad083f4ea3ec757b8e3ab
Lau <laurentiucozma12@gmail.com>
2024-03-02 14:53:29 +0200

    responsive posts component added

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -12,37 +12,24 @@ function Home() {
       <main>
         <HeroSection />
         <div
-          className=" mx-auto max-w-7xl
-                      px-6 
-                      lg:px-4 
-                      xl:grid xl:grid-cols-12 xl:gap-6 xl:pt-8
+          className=" mx-auto max-w-7xl px-3 pt-6
+                      sm:px-6 sm:pt-6
+                      lg:px-8 lg:pt-8
+                      xl:grid xl:grid-cols-12
                       2xl:px-0"
         >
-          <section id="news" className="xl:col-start-1 xl:col-end-9">
-            <Posts />
-            <Posts />
-            <Posts />
+          <section id="news" className="xl:col-span-8">
             <Posts />
           </section>
           <section
             id="youtube"
-            className="hidden xl:col-start-9 xl:col-end-13 xl:block"
+            className="hidden flex-col xl:col-span-4 xl:flex"
           >
-            <div className="flex flex-col">
-              <div className="m-auto flex h-full w-full">
-                <img
-                  src={belf}
-                  alt="blood elf"
-                  className="mb-auto max-h-full max-w-full"
-                />
-              </div>
-              <div className="m-auto flex h-full w-full">
-                <img
-                  src={arthas}
-                  alt="arthas"
-                  className="mt-auto max-h-full max-w-full"
-                />
-              </div>
+            <div className="flex-1 lg:pt-[6.5rem]">
+              <img src={belf} alt="blood elf" />
+            </div>
+            <div className="flex-initial">
+              <img src={arthas} alt="arthas" />
             </div>
           </section>
         </div>
--- src/components/pages/Posts.tsx
+++ src/components/pages/Posts.tsx
@@ -1,25 +1,101 @@
+import Post from './Post';
 import img1 from '../../assets/images/img-1.jpg';
+import img2 from '../../assets/images/img-2.jpg';
+import img3 from '../../assets/images/img-3.jpg';
+import img4 from '../../assets/images/img-4.jpg';
+import img5 from '../../assets/images/img-5.jpg';
+import img6 from '../../assets/images/img-6.jpg';
+import img7 from '../../assets/images/img-7.jpg';
+import img8 from '../../assets/images/img-8.jpg';
+import img9 from '../../assets/images/img-9.jpg';
+
 function Posts() {
+  const posts = [
+    {
+      id: 1,
+      img: img1,
+      alt: 'img1',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+    {
+      id: 2,
+      img: img2,
+      alt: 'img2',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+    {
+      id: 3,
+      img: img3,
+      alt: 'img3',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+    {
+      id: 4,
+      img: img4,
+      alt: 'img4',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+    {
+      id: 5,
+      img: img5,
+      alt: 'img5',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+    {
+      id: 6,
+      img: img6,
+      alt: 'img6',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+    {
+      id: 7,
+      img: img7,
+      alt: 'img7',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+    {
+      id: 8,
+      img: img8,
+      alt: 'img8',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+    {
+      id: 9,
+      img: img9,
+      alt: 'img9',
+      title: 'Title: Lorem ipsum dolor sit amet consectetur!',
+      excerpt:
+        'Excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam delectus aperiam alias neque optio sapiente eum vero ipsam. Et commodi obcaecati repellendus magnam modi deserunt. Hic ducimus iusto perferendis!',
+    },
+  ];
+
   return (
     <>
-      <div className="mb-8 grid grid-cols-12 rounded-lg bg-[#0f100fe6]">
-        <img
-          src={img1}
-          alt="orc"
-          className="col-start-1 col-end-5 w-full rounded-l-lg"
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          img={post.img}
+          alt={post.alt}
+          title={post.title}
+          excerpt={post.excerpt}
         />
-        <div className="col-start-5 col-end-13 p-4">
-          <h3 className="mb-1 text-xl font-bold">
-            Post Title Lorem ipsum dolor sit amet consectetur!
-          </h3>
-          <h4>
-            Post Excerpt Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Architecto, eum minus, aperiam tenetur, a soluta quasi aliquam
-            error maxime excepturi tempora ea. Deserunt saepe ipsum quas, natus
-            at quasi veritatis.
-          </h4>
-        </div>
-      </div>
+      ))}
     </>
   );
 }
